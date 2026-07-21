e4fb4d0dfc4254dce84c514672406848b2ca6fe8
p schuhmacher <contact@pschuhmacher.com>
2026-07-21 10:50:35 +0200

    prepare les routes à implementer

--- server.mjs
+++ server.mjs
@@ -12,6 +12,22 @@ app.get("/", (req, res) => {
   res.send("Bienvenue !");
 });
 
+app.get('/movies', (req, res) => {
+//A implémenter.
+});
+
+app.get('/movies/:id', (req, res) => {
+//A implémenter.
+});
+
+app.post('/movies/:id/review', (req, res) => {
+//A implémenter.
+});
+
+app.use('/recos', (req, res) => {
+//A implémenter avec QUERY
+});
+
 app.listen(port, () => {
   console.log(`Système de recommandation de films écoute sur le port ${port}`);
 });
