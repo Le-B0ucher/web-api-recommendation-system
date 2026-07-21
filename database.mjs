864ea645cfad450ff74174206811d4bd85da1fca
p schuhmacher <contact@pschuhmacher.com>
2026-07-21 10:31:15 +0200

    add db movies and modules

---
+++ database.mjs
@@ -0,0 +1,82 @@
+[
+  {
+    "id": 1,
+    "titre": "Inception"
+  },
+  {
+    "id": 2,
+    "titre": "Le Parrain"
+  },
+  {
+    "id": 3,
+    "titre": "Pulp Fiction"
+  },
+  {
+    "id": 4,
+    "titre": "Interstellar"
+  },
+  {
+    "id": 5,
+    "titre": "Forrest Gump"
+  },
+  {
+    "id": 6,
+    "titre": "Matrix"
+  },
+  {
+    "id": 7,
+    "titre": "Le Seigneur des Anneaux : La Communauté de l'Anneau"
+  },
+  {
+    "id": 8,
+    "titre": "La Ligne Verte"
+  },
+  {
+    "id": 9,
+    "titre": "Fight Club"
+  },
+  {
+    "id": 10,
+    "titre": "Gladiator"
+  },
+  {
+    "id": 11,
+    "titre": "Les Évadés"
+  },
+  {
+    "id": 12,
+    "titre": "Le Fabuleux Destin d'Amélie Poulain"
+  },
+  {
+    "id": 13,
+    "titre": "Parasite"
+  },
+  {
+    "id": 14,
+    "titre": "The Dark Knight : Le Chevalier Noir"
+  },
+  {
+    "id": 15,
+    "titre": "Le Voyage de Chihiro"
+  },
+  {
+    "id": 16,
+    "titre": "Back to the Future"
+  },
+  {
+    "id": 17,
+    "titre": "Intouchables"
+  },
+  {
+    "id": 18,
+    "titre": "La Liste de Schindler"
+  },
+  {
+    "id": 19,
+    "titre": "Le Silence des Agneaux"
+  },
+  {
+    "id": 20,
+    "titre": "Star Wars : Épisode IV - Un Nouvel Espoir"
+  }
+]
